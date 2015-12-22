fbd6e3793a726fd83d2f719a755d4a16b776daea
Derek Koloditch <derek.koloditch@gmail.com>
2015-12-21 17:27:45 -0700

    more backbone work - not fully functional yet

--- generator.clj
+++ generator.clj
@@ -1,3 +1,5 @@
+(require '[clojure.set :as set])
+
 (defn get-line-from-file [number]
   (with-open [wordlist (clojure.java.io/reader "wordlist.txt")] 
     (doall
@@ -5,5 +7,18 @@
         (apply str line)
       )
     )
+  )
+)
+
+(defn get-match [n]
+  (re-seq 
+    #"\b()[a-z]+|\d{1,4}$|([0-9]{1,4}(th|rd|st))$|[\!\@\#\$\%\^\&\*\(\)\-\_\=\+\;\:\?]{1,2}$" 
+    (apply str (get-line-from-file n))
+  )
+)
+
+(defn generate-random-numbers [n]
+  (vec
+    (let [a-set (set (take n (repeatedly #(rand-int 666666))))] a-set)
   )
 )
